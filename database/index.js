2f9b78174cbb65f77a447cce033ffdf3ca00c2fa
Grahams Laptop <doughertyg37@gmail.com>
2019-05-10 23:47:28 -0700

    implemented database with the server requests and wrote the callbacks

--- database/index.js
+++ database/index.js
@@ -1,20 +1,78 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
-var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/fetcher'); //connecting to our db
+var db = mongoose.connection; //defining a handle for our db
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => { console.log('connected to db!')});
+db.once('open', () => { //called once the connection is opened
+	console.log('connected to db!'); //now do the rest of the db setup
+	
+	//must define our schema:
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+	let repoSchema = mongoose.Schema({ //use promise instead?
+	  // schema design, add method?
+	  id: Number,
+	  name: String,
+	  url: String,
+	  owner: String
+	});
+
+	//add method here to schema? method to return data
+
+	//compile into a model for use:
+
+	let Repo = mongoose.model('Repo', repoSchema); //entries to our db will instances of Repo
+
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+//define save callback and actions
+/* wait this is wrong:
+let save = (err, repo) => {
+	if (err) {
+		console.log('error saving repo! eek!');
+		return;
+	}
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+	console.log('successfully saved repo:', repo);
+}
+*/
+
+let save = (repoToSave) => {
+	repoToSave.save((err, repo) => {
+		if (err) {
+			console.log('error saving repo! eek!');
+			return;
+		}
+
+		console.log('successfully saved repo:', repo);
+	})
 }
 
+//should define find callback here and any others we might use
+
+let find = (repo, id, callback) => {
+
+	repo.find(id, callback);
+}
+
+// let find = (err, repos) => {
+// 	if (err) {
+// 		console.log('error finding repo! please re-evaluate life choices');
+// 		return;
+// 	}
+
+// 	console.log('found repos!:', repos);
+// }
+
+/*
+
+in checking for saved repos we can user 'owner' and username as the 
+first argument to search for repos owned by that user
+additionally we can search passing 'name' or id as the first argument
+to ensure we don't create any duplicates 
+
+*/
+
+//insert comment art below
+module.exports.db = db;
+module.exports.find = find;
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,70 @@
+import getReposByUsername from '../helpers/github.js';
+import save from '../database/index.js';
+import db from '../database/index.js';
 const express = require('express');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+  /* TO-DOs
+
+  receive username on req
+  send username in request to github, receive info back
+  first check if array lengths are the same, if db is
+  shorter, replace with new data.
+  Search db for repos, save those that don't exist
+  return success message
+  
+  */
+
+  	getReposByUsername(req.username, (err, data) => {
+  		if (err) {
+  			console.log('error getting repos from github!:', err);
+  			return;
+  		}
+
+  		data.forEach((repo) => { //access each object, aka repo in the array
+  			var newDbEntry = { //defining the repo object to save to db
+  				id: repo.id,
+  				name: repo.name,
+  				url: repo.html_url,
+  				owner: repo.owner.login
+  			}
+
+
+  			find(repo, { id: newDbEntry.id }, (err, returnedRepo) => {
+  				if (err) {
+  					console.log('error finding this repo!'); //repo doesn't exist
+  					//insert repo to db
+  					var repoEntry = new Repo(newDbEntry);
+
+  					repoEntry.save(); //save new repo in db
+
+
+  					return;
+  				}
+
+  				console.log('repo already exists in db!'); //do nothing
+  			})
+
+  			if () {}
+
+
+  		})
+  	})
+
+
+
 });
 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+  /* To-Dos
+
+  query db for top 25 repos, search/sort by some value
+  possibly use options to specify 25 max results 
+	
+  */
 });
 
 let port = 1128;
