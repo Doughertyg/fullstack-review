75c292f798d08515fdaf7bfd81eea171f1e465d7
Grahams Laptop <doughertyg37@gmail.com>
2019-05-10 21:27:28 -0700

    setup database and connected to it. installed dependencies and setup my config file

--- database/index.js
+++ database/index.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => { console.log('connected to db!')});
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
